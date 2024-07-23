6c0375ec7484c647f1aa6ffbb6a061aa9dc584a3
Tad Yuh <tadyuh76@gmail.com>
2024-07-24 04:56:56 +0700

    edit page content

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex h-screen flex-col items-center justify-center p-24">
-      <h1>Thằng Nhân gay</h1>
+      <h1>© 2024 Huong Dat Huy.</h1>
     </main>
   )
 }
